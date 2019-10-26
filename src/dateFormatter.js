f47fd6b25e4b272840cb9367fac59462dfd663f0
reubengt <reubengeorgethomas@gmail.com>
2019-10-26 13:35:23 +0100

    write passing code for test case when systemDate and dateToFormat are the same date

--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -6,5 +6,11 @@
 // When the date is not current system date return a formatted date string in the format 'DD/MM/YYYY'
 
 export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return "";
+  const dateToFormatObj = new Date(dateToFormatTimeMillis);
+  const systemDateObj = new Date(systemDateTimeMillis);
+  if (dateToFormatObj.toDateString() === systemDateObj.toDateString()) {
+    return "TODAY";
+  } else {
+    return "";
+  }
 };
